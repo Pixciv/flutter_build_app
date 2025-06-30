e045581c30d79467daf32c512686dc03b8e9ff22
Pixciv <snackzmind@gmail.com>
2025-06-30 03:55:23 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();  // AdMob'u başlat
   runApp(MyApp());
 }
 
@@ -11,14 +14,68 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: appName,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(appName),
-        ),
-        body: Center(
-          child: Text('Welcome to $appName!'),
-        ),
+      home: MyHomePage(appName: appName),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String appName;
+
+  const MyHomePage({Key? key, required this.appName}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late BannerAd _bannerAd;
+  bool _isBannerAdReady = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _bannerAd = BannerAd(
+      adUnitId: 'ca-app-pub-3940256099942544/6300978111', // Test Banner ID
+      size: AdSize.banner,
+      request: AdRequest(),
+      listener: BannerAdListener(
+        onAdLoaded: (_) => setState(() {
+          _isBannerAdReady = true;
+        }),
+        onAdFailedToLoad: (ad, error) {
+          ad.dispose();
+          print('Banner reklam yüklenemedi: $error');
+        },
       ),
+    );
+
+    _bannerAd.load();
+  }
+
+  @override
+  void dispose() {
+    _bannerAd.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.appName),
+      ),
+      body: Center(
+        child: Text('Welcome to ${widget.appName}!'),
+      ),
+      bottomNavigationBar: _isBannerAdReady
+          ? Container(
+              width: _bannerAd.size.width.toDouble(),
+              height: _bannerAd.size.height.toDouble(),
+              child: AdWidget(ad: _bannerAd),
+            )
+          : SizedBox.shrink(),
     );
   }
 }
