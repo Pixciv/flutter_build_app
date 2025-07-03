c4593e5af81e36c5e9271d4eac0b009cf832e403
Pixciv <snackzmind@gmail.com>
2025-07-03 16:19:33 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,147 +1,63 @@
-import 'dart:async';
-import 'dart:math';
+import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: WebViewWithBalls(),
     debugShowCheckedModeBanner: false,
+    home: LocalHtmlWebView(),
   ));
 }
 
-class WebViewWithBalls extends StatefulWidget {
-  const WebViewWithBalls({Key? key}) : super(key: key);
+class LocalHtmlWebView extends StatefulWidget {
+  const LocalHtmlWebView({super.key});
 
   @override
-  State<WebViewWithBalls> createState() => _WebViewWithBallsState();
+  State<LocalHtmlWebView> createState() => _LocalHtmlWebViewState();
 }
 
-class _WebViewWithBallsState extends State<WebViewWithBalls>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late List<Ball> balls;
-  final int numBalls = 15;
-  late double screenWidth;
-  late double screenHeight;
-  late WebViewController _webViewController;
+class _LocalHtmlWebViewState extends State<LocalHtmlWebView> {
+  late final WebViewController _controller;
 
   @override
   void initState() {
     super.initState();
 
+    // Android için WebView platformunu ayarla
     if (Platform.isAndroid) {
       WebView.platform = SurfaceAndroidWebView();
     }
 
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(days: 1),
-    )..addListener(() {
-        updateBalls();
-      });
-
-    balls = [];
-    WidgetsBinding.instance.addPostFrameCallback((_) => initBalls());
-    _controller.repeat();
-  }
-
-  void initBalls() {
-    final rand = Random();
-    screenWidth = MediaQuery.of(context).size.width;
-    screenHeight = MediaQuery.of(context).size.height;
-
-    balls = List.generate(numBalls, (i) {
-      return Ball(
-        x: rand.nextDouble() * screenWidth,
-        y: rand.nextDouble() * screenHeight / 2,
-        dx: rand.nextDouble() * 4 - 2,
-        dy: rand.nextDouble() * 4 - 2,
-        radius: 12 + rand.nextDouble() * 8,
-        color: Colors.primaries[i % Colors.primaries.length],
-      );
-    });
-  }
-
-  void updateBalls() {
-    for (var ball in balls) {
-      ball.x += ball.dx;
-      ball.y += ball.dy;
-
-      if (ball.x < 0 || ball.x > screenWidth) ball.dx *= -1;
-      if (ball.y < 0 || ball.y > screenHeight) ball.dy *= -1;
-    }
-    setState(() {});
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted);
   }
 
   @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    // HTML dosyasını yükle
+    loadLocalHtml();
+  }
+
+  void loadLocalHtml() async {
+    final String fileHtml =
+        await DefaultAssetBundle.of(context).loadString('assets/web/index.html');
+
+    final uri = Uri.dataFromString(
+      fileHtml,
+      mimeType: 'text/html',
+      encoding: Encoding.getByName('utf-8'),
+    );
+
+    _controller.loadRequest(uri);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          WebView(
-            initialUrl: 'assets/web/index.html',
-            javascriptMode: JavascriptMode.unrestricted,
-            onWebViewCreated: (controller) {
-              _webViewController = controller;
-              _loadHtmlFromAssets();
-            },
-          ),
-          IgnorePointer(
-            child: CustomPaint(
-              size: Size.infinite,
-              painter: BallPainter(balls: balls),
-            ),
-          ),
-        ],
-      ),
+      body: WebViewWidget(controller: _controller),
     );
   }
-
-  Future<void> _loadHtmlFromAssets() async {
-    final htmlString = await DefaultAssetBundle.of(context)
-        .loadString('assets/web/index.html');
-    _webViewController.loadUrl(Uri.dataFromString(
-      htmlString,
-      mimeType: 'text/html',
-      encoding: Encoding.getByName('utf-8'),
-    ).toString());
-  }
-}
-
-class Ball {
-  double x, y, dx, dy, radius;
-  Color color;
-
-  Ball({
-    required this.x,
-    required this.y,
-    required this.dx,
-    required this.dy,
-    required this.radius,
-    required this.color,
-  });
-}
-
-class BallPainter extends CustomPainter {
-  List<Ball> balls;
-  BallPainter({required this.balls});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    for (var ball in balls) {
-      final paint = Paint()..color = ball.color.withOpacity(0.5);
-      canvas.drawCircle(Offset(ball.x, ball.y), ball.radius, paint);
-    }
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
